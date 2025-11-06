1b953ccd7e9cecf2a526bbf6c4e87cf30bb41c03
Lê Trung Hiếu <lehieutz2004@gmail.com>
2025-11-06 15:22:10 +0700

    Pull backend, add gitignore for BE

---
+++ Frontend/food-delivery/src/features/home/components/RecommendList.tsx
@@ -0,0 +1,10 @@
+const RecommendList: React.FC = () => {
+  return (
+    <div>
+      <p>Recommended Restaurants</p>
+      {/* Add recommended restaurant listings here */}
+    </div>
+  );
+};
+
+export default RecommendList;
