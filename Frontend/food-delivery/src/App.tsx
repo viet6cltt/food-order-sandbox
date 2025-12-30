468299b2a8fcc00f4171ab70dd2ab411e56307bd
Lê Trung Hiếu <lehieutz2004@gmail.com>
2025-12-30 11:16:28 +0700

    update(food): update and integrate food detail, cart screen

--- Frontend/food-delivery/src/App.tsx
+++ Frontend/food-delivery/src/App.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import { ToastContainer } from 'react-toastify';
 import { AuthProvider } from './contexts/AuthContext';
 import AppRoutes from './routes/AppRoutes';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <AuthProvider>
       <AppRoutes />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </AuthProvider>
   );
 }
