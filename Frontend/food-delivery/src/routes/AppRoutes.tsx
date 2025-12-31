7dd25ba833f4432a728589769852c3868a21d2fc
viet6cltt <phanvanviet70@gmail.com>
2025-12-31 15:22:25 +0700

    fix(auth): fix little error

--- Frontend/food-delivery/src/routes/AppRoutes.tsx
+++ Frontend/food-delivery/src/routes/AppRoutes.tsx
@@ -3,14 +3,17 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import HomeScreen from '../features/home/HomeScreen';
 import LoginScreen from '../features/auth/screens/LoginScreen';
 import SignupScreen from '../features/auth/screens/SignupScreen';
-import OwnerRegisterScreen from '../features/owner/OwnerRegisterScreen';
-import RestaurantDetailScreen from '../features/restaurant/RestaurantDetailScreen';
-import CartScreen from '../features/cart/CartScreen';
-import FoodDetailScreen from '../features/food/FoodDetailScreen';
-import PaymentScreen from '../features/payment/PaymentScreen';
+// import OwnerRegisterScreen from '../features/owner/OwnerRegisterScreen';
+// import RestaurantDetailScreen from '../features/restaurant/RestaurantDetailScreen';
+// import CartScreen from '../features/cart/CartScreen';
+// import FoodDetailScreen from '../features/food/FoodDetailScreen';
+// import PaymentScreen from '../features/payment/PaymentScreen';
 import OrderListScreen from '../features/order/OrderListScreen';
 import ResetPasswordRequestScreen from '../features/auth/screens/ResetPasswordRequestScreen';
 import ResetPasswordScreen from '../features/auth/screens/ResetPasswordScreen';
+// import ProtectedRoute from './ProtectedRoute';
+// import RoleBasedRedirect from './RoleBasedRedirect';
+// import ProfileScreen from '../features/profile/screens/ProfileScreen';
 
 const AppRoutes: React.FC = () => {
 	return (
@@ -22,13 +25,8 @@ const AppRoutes: React.FC = () => {
 				<Route path="/order-list" element={<OrderListScreen />} />
 				<Route path="/reset-password-request" element={<ResetPasswordRequestScreen />} />
 				<Route path="/reset-password" element={<ResetPasswordScreen />} />
-				<Route path="/cart" element={<CartScreen />} />
-				<Route path="/payment" element={<PaymentScreen />} />
-				<Route path="/restaurant/:restaurantId/*" element={<RestaurantDetailScreen />} />
-				<Route path="/food/:foodId" element={<FoodDetailScreen />} />
-				<Route path="/owner/register" element={<OwnerRegisterScreen />} />
-				<Route path="/orders" element={<OrderListScreen />} />
-				<Route path="*" element={<Navigate to="/" replace />} />
+				
+				 
 			</Routes>
 		</BrowserRouter>
 	);
